ef932cb002a06a9dd2773add21dc4e15a2b56eef
Mahi Singh <cloud.mahisingh@gmail.com>
2026-03-02 13:32:48 +0530

    truce

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const API_KEY = "6b076f3a79cef0971cd1cd12025fba75"
 const city = document.querySelector("#city")
 const search = document.querySelector("#search")
+const history  = document.querySelector(".serachHistory")
+let visitedCities=[]
 
-search.addEventListener("click", async (e) => {
+search.addEventListener("submit", async (e) => {
   e.preventDefault()
   const data = city.value
-  if (data) {
+  searchWeather(data)
+ 
+})
+
+async function searchWeather(city) {
+     if (city) {
       try{
-          const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${API_KEY}`,)
+          const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`,)
           const weatherData = await response.json()
           console.log(weatherData)
           if (weatherData.cod === 200){
@@ -17,8 +24,10 @@ search.addEventListener("click", async (e) => {
                <p>Humidity: ${weatherData.main.humidity}</p>
                <p>Wind: ${weatherData.wind.speed} miles/hr</p>`
 
-               visitedCities.push(data)
-               localStorage.setItem("visitedCities",JSON.stringify(visitedCities))
+               if(!visitedCities.includes(city)){
+                   visitedCities.push(city)
+                   localStorage.setItem("visitedCities",JSON.stringify(visitedCities))
+                }
             } else {
                   container.innerhtml = `<h3>Weather Information</h3>
                   <p> ${weatherData.message}</p>`
@@ -27,4 +36,20 @@ search.addEventListener("click", async (e) => {
            console.log(e)
         }
     }
-})
+    
+}
+
+function showHistory(){
+    history.innerHTML=""
+    const cities=JSON.parse(localStorage.getItem("visitedCities"))
+     if(cities)cities.forEach((city)=>{
+        const li=document.createElement("button")
+        li.textContent=city
+        li.addEventListener("click",()=>{
+            searchWeather(city)
+        })
+        history.appendChild(li)
+    })
+}
+
+showHistory()
